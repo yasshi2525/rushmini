f78f46cd629b75f43249deadbec6c6af197b601e
yasshi2525 <yasshy2@gmail.com>
2020-04-03 13:46:39 +0900

    市場ランキング対応用に変更

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-function main(param: g.GameMainParameterObject): void {
+import { GameMainParameterObject, RPGAtsumaruWindow } from "./parameterObject";
+
+declare const window: RPGAtsumaruWindow;
+
+export function main(param: GameMainParameterObject): void {
 	const scene = new g.Scene({
 		game: g.game,
 		// このシーンで利用するアセットのIDを列挙し、シーンに通知します
 		assetIds: ["player", "shot", "se"]
 	});
+	let time = 60; // 制限時間
+	if (param.sessionParameter.totalTimeLimit) {
+		time = param.sessionParameter.totalTimeLimit; // セッションパラメータで制限時間が指定されたらその値を使用します
+	}
+	// 市場コンテンツのランキングモードでは、g.game.vars.gameState.score の値をスコアとして扱います
+	g.game.vars.gameState = { score: 0 };
 	scene.loaded.add(() => {
 		// ここからゲーム内容を記述します
 
@@ -26,9 +36,44 @@ function main(param: g.GameMainParameterObject): void {
 			// プレイヤーの座標に変更があった場合、 modified() を実行して変更をゲームに通知します
 			player.modified();
 		});
+		scene.append(player);
+
+		// フォントの生成
+		const font = new g.DynamicFont({
+			game: g.game,
+			fontFamily: g.FontFamily.Serif,
+			size: 48
+		});
+
+		// スコア表示用のラベル
+		const scoreLabel = new g.Label({
+			scene: scene,
+			text: "SCORE: 0",
+			font: font,
+			fontSize: font.size / 2,
+			textColor: "black"
+		});
+		scene.append(scoreLabel);
+
+		// 残り時間表示用ラベル
+		const timeLabel = new g.Label({
+			scene: scene,
+			text: "TIME: 0",
+			font: font,
+			fontSize: font.size / 2,
+			textColor: "black",
+			x: 0.7 * g.game.width
+		});
+		scene.append(timeLabel);
 
 		// 画面をタッチしたとき、SEを鳴らします
 		scene.pointDownCapture.add(() => {
+			// 制限時間以内であればタッチ1回ごとにSCOREに+1します
+			if (time > 0) {
+				g.game.vars.gameState.score++;
+				scoreLabel.text = "SCORE: " + g.game.vars.gameState.score;
+				scoreLabel.invalidate();
+			}
 			(scene.assets["se"] as g.AudioAsset).play();
 
 			// プレイヤーが発射する弾を生成します
@@ -54,10 +99,24 @@ function main(param: g.GameMainParameterObject): void {
 			});
 			scene.append(shot);
 		});
-		scene.append(player);
+		const updateHandler = () => {
+			if (time <= 0) {
+				// RPGアツマール環境であればランキングを表示します
+				if (param.isAtsumaru) {
+					const boardId = 1;
+					window.RPGAtsumaru.experimental.scoreboards.setRecord(boardId, g.game.vars.gameState.score).then(function() {
+						window.RPGAtsumaru.experimental.scoreboards.display(boardId);
+					});
+				}
+				scene.update.remove(updateHandler); // カウントダウンを止めるためにこのイベントハンドラを削除します
+			}
+			// カウントダウン処理
+			time -= 1 / g.game.fps;
+			timeLabel.text = "TIME: " + Math.ceil(time);
+			timeLabel.invalidate();
+		};
+		scene.update.add(updateHandler);
 		// ここまでゲーム内容を記述します
 	});
 	g.game.pushScene(scene);
 }
-
-export = main;
