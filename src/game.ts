c19e425edcf4d239c1b1a82f8db15da3430a72b7
yasshi2525 <yasshy2@gmail.com>
2020-04-03 17:29:04 +0900

    路線を敷く機能を追加

--- src/game.ts
+++ src/game.ts
@@ -33,15 +33,84 @@ const getGuideE = (scene: g.Scene) => {
     }));
 
     scene.pointDownCapture.add(() => {
-        guide.destroy();
+        guide.hide();
         scene.modified();
     })
 
     return guide;
 }
 
+type Point = {x: number, y: number};
+type RailLine = {
+    chain: Point[],
+    fixed: boolean,
+};
+
+const getDrawer = (scene: g.Scene) => {
+    const primaryLine: RailLine = { chain: [], fixed: false };
+
+    const panel = new g.FilledRect({
+        scene,
+        x: 0,
+        y: 0,
+        width: g.game.width,
+        height: g.game.height,
+        cssColor: "#ffffff",
+        opacity: 0.25,
+        touchable: true,
+        tag: primaryLine
+    });
+
+    panel.pointDown.add(ev => {
+        if (panel.children) {
+            const copy: g.E[] = [];
+            panel.children.forEach(c => copy.push(c));
+            copy.forEach(c => panel.remove(c));
+        }
+        primaryLine.chain = [ev.point];
+        primaryLine.fixed = false;
+        panel.opacity = 0.25;
+        panel.modified();
+    });
+
+    panel.pointMove.add(ev => {
+        // 前イベントが発生した点へ線を結ぶ
+        const before = primaryLine.chain[primaryLine.chain.length - 1];
+        const now: Point = {
+            x: ev.point.x + ev.startDelta.x,
+            y: ev.point.y + ev.startDelta.y,
+        };
+        const d = Math.sqrt(ev.prevDelta.x * ev.prevDelta.x 
+            + ev.prevDelta.y * ev.prevDelta.y);
+        primaryLine.chain.push(now);
+        panel.append(new g.FilledRect({
+            scene,
+            x: (before.x + now.x) / 2 - 2.5,
+            y: (before.y + now.y) / 2 - 2.5,
+            width: d,
+            height: 5,
+            anchorX: 0.5,
+            anchorY: 0.5,
+            angle: Math.atan2(ev.prevDelta.y, ev.prevDelta.x) * 180 / Math.PI,
+            cssColor: "#000000",
+        }));
+        panel.modified();
+    });
+
+    panel.pointUp.add(() => {
+        primaryLine.fixed = true;
+        panel.opacity = 1.0;
+        panel.modified();
+    })
+
+    return panel;
+}
+
 export const getGameScene = () => {
     const scene = new g.Scene({ game: g.game });
+
+    scene.append(getDrawer(scene));
+    scene.append(getGuideE(scene));
 
     scene.append(new g.SystemLabel({
         scene,
@@ -51,7 +120,5 @@ export const getGameScene = () => {
         y: 30,
         textAlign: g.TextAlign.Center
     }));
-
-    scene.append(getGuideE(scene));
     return scene;
 }
