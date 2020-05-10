a40e1d55d5c3f7854b507c7477503b911f9b6e94
yasshi2525 <yasshy2@gmail.com>
2020-05-10 20:32:46 +0900

    電車で移動中にデスポーンした場合、電車の位置に合わせて動くよう修正

--- src/entities/human_despawner.ts
+++ src/entities/human_despawner.ts
@@ -1,5 +1,6 @@
 import Human, { HumanState } from "../models/human";
 import modelListener, { EventType } from "../models/listener";
+import Point from "../models/point";
 import { insertTop } from "../utils/common";
 import ticker from "../utils/ticker";
 import { adjust } from "./creator";
@@ -15,10 +16,22 @@ const handleDeleted = (container: g.E, h: Human) => {
   const panel = adjust(scene, h, sprite);
 
   let velocity = 0;
+  const initPanelLoc = new Point(panel.x, panel.y);
+  const initSubjectLoc = h.loc();
+  let dx = 0;
+  let dy = 0;
+  let effectY = 0;
   const update = () => {
     // 会社に到達した場合は死亡エフェクトを出さない
     if (h.state() === HumanState.DIED) {
-      panel.y -= velocity;
+      if (h._getTrain()) {
+        const pos = h._getTrain().loc();
+        dx = pos.x - initSubjectLoc.x;
+        dy = pos.y - initSubjectLoc.y;
+      }
+      effectY -= velocity;
+      panel.x = initPanelLoc.x + dx;
+      panel.y = initPanelLoc.y + dy + effectY;
     }
     panel.opacity -= 1 / (ANIMATION_SEC * ticker.fps());
     panel.opacity = Math.max(panel.opacity, 0);
